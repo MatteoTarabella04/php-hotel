df647efe9b3610f5d0d6abbc1c3bec6ceeeb6595
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-04 16:26:06 +0200

    create table with elements

--- index.php
+++ index.php
@@ -38,4 +38,78 @@ $hotels = [
    ],
 
 ];
+
+/* foreach ($hotels as $hotel) {
+var_dump($hotel);
+} */
 ?>
+
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+   <meta charset='UTF-8'>
+   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+   <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css' rel='stylesheet'
+      integrity='sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65' crossorigin='anonymous'>
+   <title></title>
+</head>
+
+<body>
+
+
+   <div class="container">
+
+      <table class="table m-auto">
+         <thead>
+            <tr>
+               <th scope="col">#</th>
+               <th scope="col">
+                  Name
+               </th>
+               <th scope="col">
+                  Description
+               </th>
+               <th scope="col">
+                  Parking
+               </th>
+               <th scope="col">
+                  Vote
+               </th>
+               <th scope="col">
+                  Distance to Center
+               </th>
+            </tr>
+         </thead>
+         <tbody>
+            <?php foreach ($hotels as $hotel): ?>
+               <tr>
+                  <th scope="row">.</th>
+                  <td>
+                     <?= $hotel["name"] ?>
+                  </td>
+                  <td>
+                     <?= $hotel["description"] ?>
+                  </td>
+                  <td>
+                     <?= $hotel["parking"] ?>
+                  </td>
+                  <td>
+                     <?= $hotel["vote"] ?>
+                  </td>
+                  <td>
+                     <?= $hotel["distance_to_center"] ?>
+                  </td>
+               </tr>
+            <?php endforeach; ?>
+         </tbody>
+      </table>
+
+   </div>
+
+
+
+</body>
+
+</html>
